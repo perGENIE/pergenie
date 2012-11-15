614aad758b40def05dbca10b55069aecfa4846b9
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2012-11-15 19:22:04 +0900

    add tests for login (frontend)

--- pergenie/apps/frontend/tests.py
+++ pergenie/apps/frontend/tests.py
@@ -5,12 +5,43 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+# from django.core import management
+
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SimpleTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        user = User.objects.create_user('temp', '', 'temp')
+        # user.save()
+
+        # options = dict(verbosity=0,)
+        # management.call_command('loaddata', 'task/fixtures/test.json', **options)
+
+    def test_get_index_page(self):
+        """ """
+        response = self.client.get('/', follow=True)
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_get_login_page(self):
+        """ """
+        response = self.client.get('/login/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_login(self):
+        """ """
+        self.failUnlessEqual(self.client.login(username='anonymous', password='anonymous'), False)
+        self.failUnlessEqual(self.client.login(username='temp', password='temp'), True)
+
+    def test_login_required(self):
+        """ """
+        response = self.client.get('/dashboard/')
+        self.failUnlessEqual(response.status_code, 302)
+
+        self.client.login(username='temp', password='temp')
+        response = self.client.get('/dashboard/')
+        self.failUnlessEqual(response.status_code, 200)
+
