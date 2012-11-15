c61267b874359f9793e3158319a72b7110e6863e
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2012-11-15 20:45:40 +0900

    show msg in dashboard when no genome files uploaded

--- pergenie/apps/dashboard/views.py
+++ pergenie/apps/dashboard/views.py
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*- 
+
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 
+import pymongo
 
 @login_required
 def index(request):
-    return direct_to_template(request, 'dashboard.html')
+    user_id = request.user.username
+    msg = ''
+    err = ''
+
+    with pymongo.Connection() as connection:
+        db = connection['pergenie']
+        data_info = db['data_info']
+
+        while True:
+            # determine file
+            infos = list(data_info.find( {'user_id': user_id} ))
+
+            if not infos:
+                msg = 'まずは，genome fileをアップロードしましょう!'
+                break
+
+    return direct_to_template(request,
+                              'dashboard.html',
+                              {'msg': msg, 'err': err})
