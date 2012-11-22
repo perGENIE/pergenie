4b29776f445007c78e343548e2559aa4bf70254d
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2012-11-22 19:49:27 +0900

    add path to static (urls.py)

--- pergenie/pergenie/urls.py
+++ pergenie/pergenie/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import os
+DOCUMENT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static')  ###
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pergenie.views.home', name='home'),
@@ -11,6 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': DOCUMENT_ROOT}),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
