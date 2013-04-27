d09f17b13b06ddc666db46d50d963ad8a28f4e79
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-04-28 05:37:31 +0900

    add test for faq & library.

--- pergenie/apps/faq/tests.py
+++ pergenie/apps/faq/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+# -*- coding: utf-8 -*-
 
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
+from django.conf import settings
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        # create test user
+        self.client = Client()
+        self.test_user_id = settings.TEST_USER_ID
+        self.test_user_password = settings.TEST_USER_PASSWORD
+        User.objects.create_user(self.test_user_id,
+                                 '',
+                                 self.test_user_password)
+
+    def test_success(self):
+        self.client.login(username=self.test_user_id, password=self.test_user_password)
+        response = self.client.get('/faq/')
+        self.failUnlessEqual(response.status_code, 200)
