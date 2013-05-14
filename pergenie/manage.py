4ea0b17867df921569399daeb2834b6ab87496ad
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-05-14 21:38:11 +0900

    swithch setting for staging/develep in manage.py

--- pergenie/manage.py
+++ pergenie/manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-import os
-import sys
+import sys, os
+import getpass
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pergenie.settings.develop")
+    who_am_i = getpass.getuser()
+
+    if who_am_i == 'w3pgenie':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pergenie.settings.staging")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pergenie.settings.develop")
 
     from django.core.management import execute_from_command_line
 
