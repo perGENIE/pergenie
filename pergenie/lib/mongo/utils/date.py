37358770f6415d43978c6255458f4fd00c4e199c
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-05-14 21:34:13 +0900

    fix settings for staging

--- pergenie/lib/mongo/utils/date.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import datetime
-
-today = datetime.date.today()
-today_date = datetime.datetime.strptime(str(today), '%Y-%m-%d')
-today_str = str(today).replace('-', '_')
-
-now_date = str(datetime.datetime.today()).split('.')[-2]
