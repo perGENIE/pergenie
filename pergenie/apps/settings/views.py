a6348c19a4f61f2f49d1b756856e19db91c772cd
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-05-17 23:27:52 +0900

    revise /settings

--- pergenie/apps/settings/views.py
+++ pergenie/apps/settings/views.py
@@ -1,21 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 from apps.settings.forms import SettingsForm
 
-import pymongo
+from pymongo import MongoClient
+
 
 @require_http_methods(['GET', 'POST'])
 @login_required
 def user_settings(request):
     user_id = request.user.username
-    msg = ''
-    err = ''
+    msg, err = '', ''
 
-    with pymongo.Connection() as connection:
-        db = connection['pergenie']
-        user_info = db['user_info']  #
+    with MongoClient(port=settings.MONGO_PORT) as c:
+        user_info = c['pergenie']['user_info']
 
         if request.method == 'POST':
             while True:
@@ -31,16 +32,19 @@ def user_settings(request):
                     err = 'Select show level.'
                     break
 
-                user_info.update({'user_id': user_id}, {"$set": {'risk_report_show_level': show_level}}, upsert=True)
-
+                user_info.update({'user_id': user_id},
+                                 {"$set": {'risk_report_show_level': show_level}},
+                                 upsert=True)
+                msg = 'Changes were successfully saved.'
                 break
 
-        current_settings = user_info.find_one( {'user_id': user_id })
+        current_settings = user_info.find_one({'user_id': user_id})
 
-        print current_settings
         if not current_settings:  # init
-            user_info.update({'user_id': user_id}, {"$set": {'risk_report_show_level': 'show_all'}}, upsert=True)
+            user_info.update({'user_id': user_id},
+                             {"$set": {'risk_report_show_level': 'show_all'}},
+                             upsert=True)
 
-    return direct_to_template(request,
-                              'user_settings.html',
-                              {'current_settings': current_settings})
+    return direct_to_template(request, 'user_settings.html',
+                              dict(msg=msg, err=err,
+                                   current_settings=current_settings))
