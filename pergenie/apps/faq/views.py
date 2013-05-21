c583a2cda644638ec4269e04b744c395bd03c2d0
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-05-22 04:21:02 +0900

    some fix in sentences in templetes

--- pergenie/apps/faq/views.py
+++ pergenie/apps/faq/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 @login_required
 def index(request):
-    return direct_to_template(request, 'faq/index.html')
+    return direct_to_template(request, 'faq/index.html',
+                              dict(dbsnp_version=settings.DBSNP_VERSION,
+                                   refgenome_version=settings.REFGENOME_VERSION))
