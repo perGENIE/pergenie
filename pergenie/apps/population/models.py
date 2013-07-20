7f55af94f460c85fdaec9bf2c3c389eefc7d05df
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-07-20 15:24:38 +0900

    add mock in /population

--- pergenie/apps/population/models.py
+++ pergenie/apps/population/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+
+def get_pca_snps():
+    """Get PCA SNPs of each scale.
+    """
+    pass
+
+
+def get_genotypes(snps):
+    """Get genotyes of username.filename.
+    """
+    pass
+
+
+def project_new_person(genotyes, scale):
+    """Project new person onto PCA coordinate.
+
+    args:
+    retval:
+    """
+    pass
+
+
+def get_people(scale):
+    """Get points of people in PCA coordinate.
+
+    args:
+    retval:
+    """
+    pass
--- pergenie/apps/population/views.py
+++ pergenie/apps/population/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from apps.riskreport.forms import RiskReportForm
 
 import sys, os
-import pymongo
+# import pymongo
 
 from utils import clogging
 log = clogging.getColorLogger(__name__)
@@ -17,5 +17,19 @@ log = clogging.getColorLogger(__name__)
 def index(request):
     # user_id = request.user.username
     # msg, err = '', ''
+    scale = 'global'  # PCA over Global 14 population, EastAsian, European, African or Americans.
 
-    return direct_to_template(request, 'population/index.html', {})
+
+    # TODO: create funciton
+
+    # people = get_people(scale)
+    # snps = get_pca_snps(scale)
+    # genotyes = get_genotypes(snps)
+    # person = project_new_person(genotyes, scale)
+
+    people = [[1,2], [1,3], [2,3], [1,3]]  # points (PC1, PC2). each point represents one person.
+    person = [3,3]  # one point (PC1, PC2), which represetns one person in PCA coordinate.
+
+    return direct_to_template(request, 'population/index.html', dict(scale=scale,
+                                                                     people=people,
+                                                                     person=person))
