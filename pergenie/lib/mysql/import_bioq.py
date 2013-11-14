c8e64a17e913196693905f3a7b111d306fbf7e08
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-11-14 19:04:32 +0900

    use utils.io for download .sql file

--- pergenie/lib/mysql/import_bioq.py
+++ pergenie/lib/mysql/import_bioq.py
@@ -1,6 +1,7 @@
 import sys, os
 import subprocess
 from django.conf import settings
+from utils.io import get_url_content
 from utils import clogging
 log = clogging.getColorLogger(__name__)
 
@@ -8,16 +9,23 @@ log = clogging.getColorLogger(__name__)
 def import_bioq(settings):
     # tables = ['Allele', 'AlleleFreqBySsPop', 'b137_ContigInfo', 'b137_SNPContigLoc', 'b137_SNPContigLocusId', 'b137_SNPMapInfo', 'FreqSummaryBySsPop', 'GeneIdToName', 'GtyFreqBySsPop', 'LocTypeCode', 'OrganismTax', 'Pedigree', 'PopLine', 'Population', 'SNP', 'SNPAncestralAllele', 'SnpClassCode', 'SnpFunctionCode', 'SNPSubSNPLink', 'SnpValidationCode', 'SNP_HGVS', 'UniGty', 'UniVariation', '_loc_allele_freqs', '_loc_classification_ref', '_loc_functional_representative', '_loc_genotype_freqs', '_loc_maf', '_loc_sample_information', '_loc_snp_gene_list_ref', '_loc_snp_gene_ref', '_loc_snp_gene_rep_ref', '_loc_snp_summary', '_loc_table_log', '_loc_unique_mappings_ref', '_loc_validation']
 
-    mintables = ['_loc_allele_freqs', '_loc_snp_summary', 'b137_SNPContigLoc', 'GeneIdToName']
+    mintables = ['_loc_allele_freqs', '_loc_snp_summary', 'b137_SNPContigLoc', 'GeneIdToName', '_loc_unique_mappings_ref', 'SNP']
     tables = mintables
 
     for table in tables:
         sqlfile = os.path.join(settings.BIOQ_DIR, table + '.sql.gz')
         if not os.path.exists(sqlfile):
+            if not os.path.exists(settings.BIOQ_DIR): os.makedirs(settings.BIOQ_DIR)
+
             log.info('Fetching %s' % table)
+
+            #
             url = 'http://bioq.saclab.net/query/download_table.php?db=bioq_dbsnp_human_137&table={0}&format=mysql'.format(table)
-            p0 = subprocess.Popen(["wget", "--no-check-certificate", "-O", sqlfile, url])
-            log.info('Status %s' % p0.communicate()[0])
+            get_url_content(url=url, dst=sqlfile)
+
+            #
+            # p0 = subprocess.Popen(["wget", "--no-check-certificate", "-O", sqlfile, url])
+            # log.info('Status %s' % p0.communicate()[0])
 
         log.info('Importing %s' % table)
         p1 = subprocess.Popen(["gunzip", "--to-stdout", sqlfile], stdout=subprocess.PIPE)
