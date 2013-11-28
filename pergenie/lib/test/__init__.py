8d05abbc71de043a5d25874db7005f17ed0a8a98
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2013-11-28 12:23:29 +0900

    update test case

--- pergenie/lib/test/__init__.py
+++ pergenie/lib/test/__init__.py
@@ -11,8 +11,11 @@ from django.test.client import Client
 from django.conf import settings
 from django.utils.translation import ugettext as _
 from django.utils.translation import activate as translation_activate
-from lib.utils.clogging import getColorLogger
-log = getColorLogger(__name__)
+
+from lib.api.genomes import Genomes
+genomes = Genomes()
+# from lib.utils.clogging import getColorLogger
+# log = getColorLogger(__name__)
 
 
 class LoginUserTestCase(TestCase):
@@ -73,9 +76,9 @@ class LoginUserTestCase(TestCase):
             db = c['pergenie']
             data_info = db['data_info']
 
-            # delete collection `variants.user_id.filename`
-            db.drop_collection('variants.{0}.{1}'.format(self.test_user_id, self.file_cleaned_name))
-            db.drop_collection('variants.{0}.{1}'.format(self.test_user_id, self.file_cleaned_name_2))
+            # delete collection
+            db.drop_collection(genomes.get_variants(self.test_user_id, self.file_cleaned_name_1).name)
+            db.drop_collection(genomes.get_variants(self.test_user_id, self.file_cleaned_name_2).name)
 
             # because it is just a test, no need to delete `file`
 
@@ -89,30 +92,33 @@ class LoginUserTestCase(TestCase):
             user_info.update({'user_id': self.test_user_id},
                              {'$set': {'last_viewed_file': ''}}, upsert=True)
 
-    def _test_login_required(self, app_name):
+    def _test_login_required(self, url):
         # attempt to access /app_name/ without login
         # -> redirect to /login/?next=/app_name/
-        response = self.client.get('/{app_name}/'.format(app_name=app_name))
+        response = self.client.get(url)
         self.failUnlessEqual(response.status_code, 302)
 
         # attempt to access /app_name/ with login
         # but, user does not exist
         self.client.login(username=self.dummy_user_id, password='anonymousLogin')
-        response = self.client.get('/{app_name}/'.format(app_name=app_name))
+        response = self.client.get(url)
         self.failUnlessEqual(response.status_code, 302)
 
         # but, incorrect password
         self.client.login(username=self.test_user_id, password='incorrectPassword')
-        response = self.client.get('/{app_name}/'.format(app_name=app_name))
+        response = self.client.get(url)
         self.failUnlessEqual(response.status_code, 302)
 
         # success
         self.client.login(username=self.test_user_id, password=self.test_user_password)
-        response = self.client.get('/{app_name}/'.format(app_name=app_name))
+        response = self.client.get(url)
         self.failUnlessEqual(response.status_code, 200)
+
+        # logout
+        self.client.logout()
 
     def _test_logout(self, app_name):
         self.client.login(username=self.test_user_id, password=self.test_user_password)
-        response = self.client.get('/{app_name}/'.format(app_name=app_name))
+        response = self.client.get(url)
         response = self.client.get('/logout/')# , follow=True)
         self.failUnlessEqual(response.status_code, 302)
