724dbacad440d304b47536417d227fc5d809e04b
Kensuke Numakura <numakura@sb.ecei.tohoku.ac.jp>
2014-01-24 14:48:48 +0900

    add testcase

---
+++ pergenie/lib/api/test_riskreport_cui_rs519113_testcase.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+mkdir tmp
+
+# normal & without rs ID
+for zyg in CC CG GG NA; do \
+    for src in .vcf _without_rsID.vcf; do \
+        echo $zyg $src
+        $WORKON_HOME/py26perGENIE/bin/python \
+         riskreport_cui.py \
+         -I rs519113_testcase/rs519113_${zyg}${src} \
+         -F vcf_whole_genome \
+         -O tmp/${zyg}.out \
+         -P Japanese
+        grep Alz tmp/$zyg.out > tmp/$zyg.Alz.out
+        diff tmp/$zyg.Alz.out rs519113_testcase/true_outputs/$zyg.Alz.out
+    done
+done
+
+# gzipped
+for zyg in CC CG GG NA; do \
+    for src in .vcf.gz; do \
+        echo $zyg $src
+        $WORKON_HOME/py26perGENIE/bin/python \
+         riskreport_cui.py \
+         -I rs519113_testcase/rs519113_${zyg}${src} \
+         -F vcf_whole_genome \
+         -O tmp/${zyg}.out \
+         -P Japanese \
+         --compress gzip
+        grep Alz tmp/$zyg.out > tmp/$zyg.Alz.out
+        diff tmp/$zyg.Alz.out rs519113_testcase/true_outputs/$zyg.Alz.out
+    done
+done
+
+rm -r tmp
