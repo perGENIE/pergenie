2ed75aa7004aae260a417a9725592ba6e458d939
knmkr <knmkr3gma@gmail.com>
2015-05-30 09:34:04 +0900

    django 1.8

--- pergenie/apps/landing/views.py
+++ pergenie/apps/landing/views.py
@@ -1,8 +1,7 @@
 import sys, os
-from django.shortcuts import redirect
-from django.views.generic.simple import direct_to_template
+from django.shortcuts import render
 from django.conf import settings
 
 
 def index(request):
-    return direct_to_template(request, 'index.html', dict())
+    return render(request, 'index.html', dict())
