31e12d78c6d5a1637c0abea7a8257ce2b0e33321
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-28 15:33:43 +0900

    wip: command for update gwascatalog

--- pergenie/apps/gwascatalog/management/commands/update_gwascatalog.py
+++ pergenie/apps/gwascatalog/management/commands/update_gwascatalog.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import csv
-import datetime
+from datetime import datetime
 
 from django.core.management.base import BaseCommand, CommandError
+from django.db import models, transaction
 from django.conf import settings
+from django.utils import timezone
+from django.utils.dateparse import parse_date
 
 from apps.gwascatalog.models import GwasCatalogSnp
 from lib.utils.io import get_url_content
@@ -15,64 +18,71 @@ log = clogging.getColorLogger(__name__)
 class Command(BaseCommand):
     help = "Update GWAS Catalog"
 
+    @transaction.atomic
     def handle(self, *args, **options):
-        try:
-            log.info('Fetching latest gwascatalog...')
+        log.info('Fetching latest gwascatalog...')
 
-            if not os.path.exists(GWASCATALOG_DIR):
-                os.makedirs(GWASCATALOG_DIR)
+        if not os.path.exists(settings.GWASCATALOG_DIR):
+            os.makedirs(settings.GWASCATALOG_DIR)
 
-            today = datetime.datetime.now().strftime('%Y-%m-%d')
-            catalog_path = os.path.join(GWASCATALOG_DIR, 'gwascatalog.{}.tsv'.format(today))
+        today = timezone.now().strftime('%Y-%m-%d')
+        catalog_path = os.path.join(settings.GWASCATALOG_DIR, 'gwascatalog_cleaned_{}.tsv'.format(today))
 
-            if not os.path.exists(catalog_path):
-                try:
-                    get_url_content(url=settings.GWASCATALOG_URL, dst=catalog_path)
-                except (IOError,KeyboardInterrupt) as exception:
-                    if os.path.exists(catalog_path):
-                        os.remove(catalog_path)
-                    raise
+        log.info('Importing gwascatalog...')
 
-            log.info('Importing gwascatalog...')
+        chunk_length = 1000
+        snps = []
 
-            chunk_length = 1000
-            gwascatalog_snps = []
+        current_tz = timezone.get_current_timezone()
+        log.info(current_tz)
 
-            reader = csv.DictReader(catalog_path, delimiter='\t')
-            for record in reader:
+        model_fields = [field for field in GwasCatalogSnp._meta.get_fields() if field.name not in ('id', 'created_at')]
+        model_field_names = [field.name for field in model_fields]
+        model_fields_map = dict(zip(model_field_names, model_fields))
 
-                population = get_population(record['initial_sample_size'])
+        reader = csv.DictReader(open(catalog_path, 'rb'), delimiter='\t')
+        for record in reader:
+            # population = get_population(record['initial_sample_size'])
 
-                if population:
-                    population_1st = population[0]
-                    db_allele_freq = {'Asian': {}, 'European': {}, 'African': {}}[population_1st]  # TODO
-                else:
-                    db_allele_freq = {}
+            # if population:
+            #     population_1st = population[0]
+            #     db_allele_freq = {'Asian': {}, 'European': {}, 'African': {}}[population_1st]  # TODO
+            # else:
+            #     db_allele_freq = {}
 
-                # TODO: odds_ratio/beta_coeff
+            # TODO: odds_ratio/beta_coeff
 
-                record.update({'risk_allele_forward': get_forward_risk_allele(record['risk_allele'],
-                                                                              record['risk_allele_freq_reported'],
-                                                                              db_allele_freq,
-                                                                              settings.GWASCATALOG_INCONSISTENCE_THRS),
-                               'population': population,
-                               'reliability_rank': get_reliability_rank(record['study'],
-                                                                        record['p_value'])}
+            record.update({'risk_allele_forward': '',
+                           'population': [],
+                           'reliability_rank': '',
+                           'odds_ratio': None,
+                           'beta_coeff': None})
 
-                snp = GwasCatalogSnp(**record)
-                gwascatalog_snps.append(snp)
+            data = {}
+            for k,v in record.items():
+                if k in model_field_names:
+                    # blank or null
+                    if v == '':
+                        if type(model_fields_map[k]) in (models.fields.CharField, models.fields.TextField):
+                            v = ''
+                        else:
+                            v = None
 
-                if len(gwascatalog_snps) == chunk_length:
-                    GwasCatalogSnp.object.bulk_create(gwascatalog_snps)
-                    gwascatalog_snps[:] = []
+                    # datetime with timezone
+                    if type(model_fields_map[k]) == models.DateTimeField:
+                        v = current_tz.localize(datetime(*(parse_date(v).timetuple()[:5])))
 
-            if len(gwascatalog_snps) > 0:
-                GwasCatalogSnp.object.bulk_create(gwascatalog_snps)
+                    data[k] = v
 
-            log.info('Done.')
+            snps.append(GwasCatalogSnp(**data))
 
-        except Exception as exception:
-            # TODO: rollback
-            # catalog.delete_catalog()
-            # catalog.delete()
-            raise
+            if len(snps) == chunk_length:
+                log.debug('processed: {} records'.format(len(snps)))
+                GwasCatalogSnp.objects.bulk_create(snps)
+                snps[:] = []
+
+        if len(snps) > 0:
+            log.debug('processed: {} records'.format(len(snps)))
+            GwasCatalogSnp.objects.bulk_create(snps)
+
+        log.info('Done.')
--- pergenie/apps/gwascatalog/models.py
+++ pergenie/apps/gwascatalog/models.py
@@ -2,60 +2,40 @@ import uuid
 import datetime
 
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from django.conf import settings
 from django.utils.translation import ugettext as _
+from django.utils import timezone
 
 from lib.utils import clogging
 log = clogging.getColorLogger(__name__)
 
+DECIMAL_PLACES_MAX = 1000
 
 class GwasCatalogSnp(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(default=datetime.datetime.now)
-    is_active = models.BooleanField(default=False)
+    created_at                     = models.DateTimeField(default=timezone.now)
+    is_active                      = models.BooleanField(default=False)
 
-    # date_added                     = models.DateTimeField()
-    pubmed_id                      = models.CharField()
-    # first_author                   = models.CharField()
-    # date_published                 = models.DateTimeField()
-    # journal                        = models.CharField()
-    # pubmed_url                     = models.CharField()
-    # study_title                    = models.CharField()
-    disease_or_trait               = models.CharField()
-    initial_sample                 = models.CharField()
-    # replication_sample             = models.CharField()
-    # region                         = models.CharField()
-    chrom                          = models.CharField()
-    pos                            = models.IntegerField()
-    gene_reported                  = models.CharField()
-    # gene_mapped                    = models.CharField()
-    # upstream_entrez_gene_id        = models.CharField()
-    # downstream_entrez_gene_id      = models.CharField()
-    # entrez_gene_id                 = models.CharField()
-    # upstream_gene_distance_kb      = models.FloatField()
-    # downstream_gene_distance_kb    = models.FloatField()
-    # strongest_snp_risk_allele      = models.CharField()
-    # strongest_snps                 = models.CharField()
-    # is_snp_id_merged               = models.NullBooleanField()
-    # snp_id_current                 = models.CharField()
-    # snp_context                    = models.CharField()
-    # is_snp_intergenic              = models.NullBooleanField()
-    risk_allele_freq_reported      = models.FloatField()
-    p_value                        = models.DecimalField()
-    # minus_log_p_value              = models.FloatField()
-    p_value_text                   = models.CharField()
-    odds_ratio_or_beta_coeff       = models.DecimalField()
-    confidence_interval_95_percent = models.CharField()
-    # snp_platform                   = models.CharField()
-    # cnv                            = models.CharField()
-    snp_id                         = models.IntegerField()
-    risk_allele                    = models.CharField(max_length=1024)
     date_downloaded                = models.DateTimeField()
+    pubmed_id                      = models.CharField(max_length=8)
+    disease_or_trait               = models.CharField(max_length=1024)
+    snp_id                         = models.IntegerField()
+    risk_allele                    = models.CharField(max_length=1024, blank=True)
 
-    reliability_rank               = models.FloatField()
-    population                     = models.CharField()
-    odds_ratio                     = models.FloatField()
-    beta_coeff                     = models.FloatField()
+    initial_sample                 = models.CharField(max_length=1024, blank=True)
+    chrom_reported                 = models.CharField(max_length=2, blank=True)
+    pos_reported                   = models.IntegerField(null=True)
+    gene_reported                  = models.CharField(max_length=1024, blank=True)
+    risk_allele_freq_reported      = models.FloatField(null=True)
+    p_value                        = models.DecimalField(max_digits=DECIMAL_PLACES_MAX, decimal_places=DECIMAL_PLACES_MAX, null=True)
+    p_value_text                   = models.CharField(max_length=1024, blank=True)
+    odds_ratio_or_beta_coeff       = models.DecimalField(max_digits=8, decimal_places=4, null=True)
+    confidence_interval_95_percent = models.CharField(max_length=1024, blank=True)
+
+    reliability_rank               = models.FloatField(null=True)
+    population                     = ArrayField(models.CharField(max_length=1024, blank=True))
+    odds_ratio                     = models.DecimalField(max_digits=8, decimal_places=4, null=True)
+    beta_coeff                     = models.DecimalField(max_digits=8, decimal_places=4, null=True)
 
     class Meta:
         unique_together = ('date_downloaded', 'pubmed_id', 'disease_or_trait', 'snp_id', 'risk_allele')
