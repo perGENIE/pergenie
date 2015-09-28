4356cd51dd6df89ecc0cc7803026f4b1d3c5d32b
Kensuke Numakura <knmkr3gma@gmail.com>
2015-09-28 17:55:39 +0900

    code formatting

--- pergenie/apps/gwascatalog/models.py
+++ pergenie/apps/gwascatalog/models.py
@@ -1,7 +1,7 @@
 import uuid
 import datetime
 
-from django.db import models
+from django.db.models import Model, DateTimeField, BooleanField, CharField, IntegerField, FloatField, DecimalField
 from django.contrib.postgres.fields import ArrayField
 from django.conf import settings
 from django.utils.translation import ugettext as _
@@ -10,32 +10,30 @@ from django.utils import timezone
 from lib.utils import clogging
 log = clogging.getColorLogger(__name__)
 
-DECIMAL_PLACES_MAX = 1000
+class GwasCatalogSnp(Model):
+    created_at                     = DateTimeField(         default=timezone.now)
+    is_active                      = BooleanField(          default=False)
 
-class GwasCatalogSnp(models.Model):
-    created_at                     = models.DateTimeField(default=timezone.now)
-    is_active                      = models.BooleanField(default=False)
+    date_downloaded                = DateTimeField()
+    pubmed_id                      = CharField(             max_length=8)
+    disease_or_trait               = CharField(             max_length=1024)
+    snp_id                         = IntegerField()
+    risk_allele                    = CharField(             max_length=1024,                          blank=True)
 
-    date_downloaded                = models.DateTimeField()
-    pubmed_id                      = models.CharField(max_length=8)
-    disease_or_trait               = models.CharField(max_length=1024)
-    snp_id                         = models.IntegerField()
-    risk_allele                    = models.CharField(max_length=1024, blank=True)
+    initial_sample                 = CharField(             max_length=1024,                          blank=True)
+    chrom_reported                 = CharField(             max_length=2,                             blank=True)
+    pos_reported                   = IntegerField(                                                    null=True)
+    gene_reported                  = CharField(             max_length=1024,                          blank=True)
+    risk_allele_freq_reported      = FloatField(                                                      null=True)
+    p_value                        = DecimalField(          max_digits=1000, decimal_places=1000,     null=True)
+    p_value_text                   = CharField(             max_length=1024,                          blank=True)
+    odds_ratio_or_beta_coeff       = DecimalField(          max_digits=8, decimal_places=4,           null=True)
+    confidence_interval_95_percent = CharField(             max_length=1024,                          blank=True)
 
-    initial_sample                 = models.CharField(max_length=1024, blank=True)
-    chrom_reported                 = models.CharField(max_length=2, blank=True)
-    pos_reported                   = models.IntegerField(null=True)
-    gene_reported                  = models.CharField(max_length=1024, blank=True)
-    risk_allele_freq_reported      = models.FloatField(null=True)
-    p_value                        = models.DecimalField(max_digits=DECIMAL_PLACES_MAX, decimal_places=DECIMAL_PLACES_MAX, null=True)
-    p_value_text                   = models.CharField(max_length=1024, blank=True)
-    odds_ratio_or_beta_coeff       = models.DecimalField(max_digits=8, decimal_places=4, null=True)
-    confidence_interval_95_percent = models.CharField(max_length=1024, blank=True)
-
-    reliability_rank               = models.FloatField(null=True)
-    population                     = ArrayField(models.CharField(max_length=1024, blank=True))
-    odds_ratio                     = models.DecimalField(max_digits=8, decimal_places=4, null=True)
-    beta_coeff                     = models.DecimalField(max_digits=8, decimal_places=4, null=True)
+    reliability_rank               = FloatField(                                                      null=True)
+    population                     = ArrayField(CharField(  max_length=1024,                          blank=True))
+    odds_ratio                     = DecimalField(          max_digits=8, decimal_places=4,           null=True)
+    beta_coeff                     = DecimalField(          max_digits=8, decimal_places=4,           null=True)
 
     class Meta:
         unique_together = ('date_downloaded', 'pubmed_id', 'disease_or_trait', 'snp_id', 'risk_allele')
