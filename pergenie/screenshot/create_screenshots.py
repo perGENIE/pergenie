17fa3bfd04bf3f7b00fe1a0ab92537e03e5d20d5
knmkr <knmkr3gma@gmail.com>
2015-10-24 19:47:11 +0900

    fix screenshots path. add TODO

--- pergenie/screenshot/create_screenshots.py
+++ pergenie/screenshot/create_screenshots.py
@@ -7,13 +7,15 @@ def get_browser(width, heigth):
     browser.driver.set_window_size(width, heigth)
     return browser
 
-def create_screenshot(browser, path, dst_dir):
+def create_screenshot(browser, path):
     base_url = 'http://localhost:8888/'
     browser.visit(os.path.join(base_url, path))
-    filename = os.path.join(dst_dir, 'screenshot{}.png'.format(path.replace('/', '-')))
+    filename = os.path.join('screenshot', 'screenshot{}.png'.format(path.replace('/', '-')))
     browser.driver.save_screenshot(filename)
 
 if __name__ == '__main__':
     browser_window_size = {'OSX': (1280, 800)}
     browser = get_browser(*browser_window_size['OSX'])
-    create_screenshot(browser, '', os.path.join('apps', 'landing', 'screenshot'))
+    create_screenshot(browser, '')
+
+    # TODO: screenshots of logged-in pages
