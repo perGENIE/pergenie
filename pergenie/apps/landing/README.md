c9d8a632b1240eb03496d3078b3a5d09a1cf9190
knmkr <knmkr3gma@gmail.com>
2015-10-24 19:24:49 +0900

    add screenshots

---
+++ pergenie/apps/landing/README.md
@@ -0,0 +1,1 @@
+![screenshot](https://github.com/knmkr/perGENIE-private/blob/master/pergenie/apps/landing/screenshot/screenshot.png)
---
+++ pergenie/screenshot/create_screenshots.py
@@ -0,0 +1,19 @@
+import os
+
+from splinter import Browser
+
+def get_browser(width, heigth):
+    browser = Browser('phantomjs')
+    browser.driver.set_window_size(width, heigth)
+    return browser
+
+def create_screenshot(browser, path, dst_dir):
+    base_url = 'http://localhost:8000/'
+    browser.visit(os.path.join(base_url, path))
+    filename = os.path.join(dst_dir, 'screenshot{}.png'.format(path.replace('/', '-')))
+    browser.driver.save_screenshot(filename)
+
+if __name__ == '__main__':
+    browser_window_size = {'OSX': (1280, 800)}
+    browser = get_browser(*browser_window_size['OSX'])
+    create_screenshot(browser, '', os.path.join('apps', 'landing', 'screenshot'))
