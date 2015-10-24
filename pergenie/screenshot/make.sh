e0abf9f1d652868c1ebfc26e2a80d3b9febc1237
knmkr <knmkr3gma@gmail.com>
2015-10-24 19:36:19 +0900

    change port 8888

--- pergenie/screenshot/make.sh
+++ pergenie/screenshot/make.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+# USAGE: ./screenshot/make.sh
 # NOTE: Need to be run on django project root.
 
 make node_modules/phantomjs
 export PATH=./node_modules/phantomjs/bin:$PATH
 
 . ./.virtualenv/perGENIE/bin/activate
-python manage.py runserver &
+python manage.py runserver 8888 &
 python screenshot/create_screenshots.py
 
 kill -TERM -$$
