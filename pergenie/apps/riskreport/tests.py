72c62955826c6ea3da4a270d2a1c68f669fc189a
Kensuke Numakura <knmkr3gma@gmail.com>
2015-11-16 14:39:29 +0900

    WIP: reimpliment using models

--- pergenie/apps/riskreport/tests.py
+++ pergenie/apps/riskreport/tests.py
@@ -1,92 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import sys, os
+import sys
+import os
 import time
 import datetime
-from pymongo import MongoClient
 
 from django.conf import settings
 from django.utils.translation import ugettext as _
 from django.utils.translation import activate as translation_activate
-from lib.mongo.import_variants import import_variants
+
 from lib.test import LoginUserTestCase
 from utils.clogging import getColorLogger
 log = getColorLogger(__name__)
 
 
 class SimpleTest(LoginUserTestCase):
-    def _setUp(self):
-        pass
-
-    def _tearDown(self):
-        pass
-
-    # def _import_data(self):
-    #     """Import genome data for test.
-    #     """
-
-    #     with MongoClient(host=settings.MONGO_URI) as c:
-    #         catalog_cover_rate = c['pergenie']['catalog_cover_rate']
-    #         data_info = c['pergenie']['data_info']
-
-    #         # Import data #1
-    #         info = {'user_id': self.test_user_id,
-    #                 'name': self.file_cleaned_name,
-    #                 'raw_name': self.file_raw_name,
-    #                 'date': datetime.datetime.today(),
-    #                 'population': self.file_population,
-    #                 'file_format': 'andme',
-    #                 'catalog_cover_rate': catalog_cover_rate.find_one({'stats': 'catalog_cover_rate'})['values']['andme'],
-    #                 'genome_cover_rate': catalog_cover_rate.find_one({'stats': 'genome_cover_rate'})['values']['andme'],
-    #                 'status': float(0.0)}
-    #         data_info.insert(info)
-
-    #         import_variants(file_path=self.file_raw_path,
-    #                         population='unknown',
-    #                         file_format='andme',
-    #                         user_id=self.test_user_id)
-
-    #         # Import data #2
-    #         info = {'user_id': self.test_user_id,
-    #                 'name': self.file_cleaned_name_2,
-    #                 'raw_name': self.file_raw_name_2,
-    #                 'date': datetime.datetime.today(),
-    #                 'population': self.file_population_2,
-    #                 'file_format': 'andme',
-    #                 'catalog_cover_rate': catalog_cover_rate.find_one({'stats': 'catalog_cover_rate'})['values']['vcf_whole_genome'],
-    #                 'genome_cover_rate': catalog_cover_rate.find_one({'stats': 'genome_cover_rate'})['values']['vcf_whole_genome'],
-    #                 'status': float(0.0)}
-    #         data_info.insert(info)
-
-    #         import_variants(file_path=self.file_raw_path_2,
-    #                         population='unknown',
-    #                         file_format='vcf_whole_genome',
-    #                         user_id=self.test_user_id)
-
-    # def _delete_data(self):
-    #     """Delete *test data*.
-    #     """
-
-    #     with MongoClient(host=settings.MONGO_URI) as c:
-    #         db = c['pergenie']
-    #         data_info = db['data_info']
-
-    #         # delete collection `variants.user_id.filename`
-    #         db.drop_collection('variants.{0}.{1}'.format(self.test_user_id, self.file_cleaned_name))
-    #         db.drop_collection('variants.{0}.{1}'.format(self.test_user_id, self.file_cleaned_name_2))
-
-    #         # because it is just a test, no need to delete `file`
-
-    #         # delete document in `data_info`
-    #         founds = list(data_info.find({'user_id': self.test_user_id}))
-    #         if founds:
-    #             for found in founds:
-    #                 data_info.remove(found)
-
-    #         user_info = c['pergenie']['user_info']
-    #         user_info.update({'user_id': self.test_user_id},
-    #                          {'$set': {'last_viewed_file': ''}}, upsert=True)
-
     def test_login_required(self):
         response = self.client.get('/riskreport/')
         self.assertEqual(response.status_code, 302)
