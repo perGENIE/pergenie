f50b0f3694b11b9b0f3722abac65b237cf058d6d
Kensuke Numakura <knmkr3gma@gmail.com>
2015-11-16 13:48:38 +0900

    remove pymongo lines

--- pergenie/apps/_myprotain/views.py
+++ pergenie/apps/_myprotain/views.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 from django.http import Http404
@@ -9,10 +7,7 @@ from apps.riskreport.forms import RiskReportForm
 
 import sys, os
 import re
-# import pymongo
 
-# from lib.mongo.get_latest_catalog import get_latest_catalog
-# from lib.mongo.get_traits_infos import get_traits_infos
 from models import *
 
 from utils import clogging
