78d4ab4bcf5d451a9ed702c02a26f4d34ebf09f4
Kensuke Numakura <knmkr3gma@gmail.com>
2015-11-19 15:11:57 +0900

    switch ROLLOUT_ENV in wsgi.py

--- pergenie/pergenie/wsgi.py
+++ pergenie/pergenie/wsgi.py
@@ -15,7 +15,8 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pergenie.settings.development")
+ROLLOUT_ENV = os.environ.get('ROLLOUT_ENV') or 'development'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pergenie.settings.{}".format(ROLLOUT_ENV))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
