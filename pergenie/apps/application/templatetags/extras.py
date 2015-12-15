177b45750f19ffbda7f2cf6b2c80d7ed82c3d480
Kensuke Numakura <knmkr3gma@gmail.com>
2015-12-15 17:51:31 +0900

    fix apps in menu bar

--- pergenie/apps/application/templatetags/extras.py
+++ pergenie/apps/application/templatetags/extras.py
@@ -1,23 +1,21 @@
-# -*- coding: utf-8 -*-
-
 """
 To use filters bellow, put `{% load extras %}` in templates.
 """
 
 from django import template
-from django.template.defaultfilters import stringfilter  # 第一引数に文字列しかとらない
+from django.template.defaultfilters import stringfilter  # only expects a string as the first argument
 from django.conf import settings
 import math
 
+
 register = template.Library()
 
-
-"""
-Django 'set variable' support
-from http://www.soyoucode.com/2011/set-variable-django-template
-"""
-
 class SetVarNode(template.Node):
+    """Django 'set variable' support
+
+    See http://www.soyoucode.com/2011/set-variable-django-template
+    """
+
     def __init__(self, var_name, var_value):
         self._var_name = var_name
         self._var_value = var_value
@@ -49,32 +47,10 @@ def limit(s, length):
     else:
         return s[:int(length)] + '...'
 
-
-# from xml.sax.saxutils import unescape, escape
-# @register.filter
-# @stringfilter
-# def unescape_html(s):
-#     return unescape(s)
-
-
-
-@register.filter
-@stringfilter
-def space2underbar(s):
-    return s.replace(' ', '_')
-
-@register.filter
-@stringfilter
-def underbar2space(s):
-    return s.replace('_', ' ')
-
-
-#
 @register.filter
 def keyvalue(dict, key):
     return dict[key]
 
-#
 @register.filter
 def listvalue(list, index):
     return list[index]
@@ -94,29 +70,6 @@ def abs(s):
 
     return math.fabs(value)
 
-# @stringfilter
-# @register.filter
-# def file_format_long_name(s):
-#     # e.g.: map `vcf_whole_genome` to `VCF (Whole Genome)`
-#     return settings.GENOME_FILE_FORMAT_LONG_NAME_MAP.get(s,s)
-
-@stringfilter
-@register.filter
-def demouser_format(s):
-    if s.startswith(settings.DEMO_USER_ID):
-        s = settings.DEMO_USER_ID
-
-    return s
-
-# @stringfilter
-# @register.filter
-# def rm_newlines(s):
-#     return ''.join(s.splitlines())
-
-# @register.filter
-# def one_value(dict):
-#     return dict.get(dict.keys()[0])
-@register.filter
 def dict_index(dict, index):
     return dict.get(dict.keys()[index])
 
@@ -127,6 +80,6 @@ def hide_None(s):
 
 @register.filter
 @stringfilter
-def is_in_installed_my_apps(s):
-    installed_my_apps = [x[5:] for x in settings.INSTALLED_APPS if x.startswith('apps.')]
-    return s in installed_my_apps
+def is_in_installed_apps(s):
+    installed_apps = [x for x in settings.INSTALLED_APPS if x.startswith('apps.')]
+    return 'apps.' + s in installed_apps
