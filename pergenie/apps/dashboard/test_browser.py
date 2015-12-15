0d147e38cb61ec13a02cec2c14d7a933a3f471dd
Kensuke Numakura <knmkr3gma@gmail.com>
2015-12-15 17:16:07 +0900

    fix tests of /dashbaord

---
+++ pergenie/apps/dashboard/test_browser.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from splinter import Browser
+import pytest
+
+from test.utils import auth
+
+
+class DashboardTestCase(TestCase):
+    def setUp(self):
+        self.test_user_id = 'test-user@pergenie.org'
+        self.test_user_password = 'test-user-password'
+        self.user = auth.create_user(self.test_user_id, self.test_user_password, is_active=True)
+
+        self.browser = auth.browser_login(Browser('django'), self.test_user_id, self.test_user_password)
+
+    def test_login_required(self):
+        self.browser.visit('/dashboard')
+        assert 'dashboard' in self.browser.title.lower()
+
+    def test_menu_bar_contains_user_id(self):
+        assert self.browser.find_by_id('menu_user_id')[0].text.replace(' ', '').strip() == self.test_user_id
--- pergenie/test/utils/auth.py
+++ pergenie/test/utils/auth.py
@@ -7,6 +7,7 @@ from apps.authentication.models import User
 
 def create_user(test_user_id, test_user_password, is_active=False):
     user = User.objects.create_user(test_user_id,
+                                    test_user_id,
                                     test_user_password)
     user.is_active = is_active
     user.save()
