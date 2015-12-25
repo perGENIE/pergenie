ecc3d51905567610314f03f1061a68ea2dd5d76f
Kensuke Numakura <knmkr3gma@gmail.com>
2015-12-25 11:57:10 +0900

    fix test

--- pergenie/test/utils/auth.py
+++ pergenie/test/utils/auth.py
@@ -2,13 +2,15 @@ from splinter import Browser
 
 from django.conf import settings
 
-from apps.authentication.models import User
+from apps.authentication.models import User, UserGrade
 
 
 def create_user(test_user_id, test_user_password, is_active=False):
+    default_user_grade, _ = UserGrade.objects.get_or_create()
     user = User.objects.create_user(test_user_id,
                                     test_user_id,
-                                    test_user_password)
+                                    test_user_password,
+                                    grade=default_user_grade)
     user.is_active = is_active
     user.save()
     return user
